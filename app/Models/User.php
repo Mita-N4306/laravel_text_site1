7b9d36dd45aaf6807b4b1461af8ac772041e7693
naoki mitamura <zango3333@yahoo.co.jp>
2024-01-02 03:04:24 +0900

    メール認証機能(2024.1.2)

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+use App\Notifications\NewVerifyEmail;
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -53,5 +53,8 @@ class User extends Authenticatable implements MustVerifyEmail
     //リレーション(多対多のメソッド)
     public function roles() {
      return $this->belongsToMany(Role::class);
+    }
+    public function sendEmailVerificationNotification() {
+     $this->notify(new NewVerifyEmail());
     }
 }
